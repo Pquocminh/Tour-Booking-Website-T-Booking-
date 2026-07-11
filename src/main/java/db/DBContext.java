b7979f930e2b0504ac36b2c11faefa6ea2edd42a
QuocHuy <huyhq.ce190273@gmail.com>
2026-07-11 21:33:08 +0700

    HuyHQ_Cancel Booking_ADD

--- src/main/java/db/DBContext.java
+++ src/main/java/db/DBContext.java
@@ -24,6 +24,18 @@ public class DBContext {
     }
 
     public Connection getConnection() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                conn = DriverManager.getConnection(
+                    "jdbc:sqlserver://localhost:1433;databaseName=BookingTourWebsite;encrypt=false",
+                    "sa",
+                    "123"
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return conn;
     }
 }
